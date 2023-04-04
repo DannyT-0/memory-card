83669c6c0ae8efbe61afd77fc5f6dd00f9e4d0fe
danny <dannytokarev@gmail.com>
2023-04-04 16:23:02 -0400

    changed files around so that app file is main function file and components only hold certain data

--- memory-card/src/App.js
+++ memory-card/src/App.js
@@ -2,10 +2,37 @@ import React from "react";
 import Card from "./Components/Card";
 import Header from "./Components/Header";
 import Score from "./Components/Score";
+import characters from "./Components/characterData";
 
 function App() {
 	function handleClick() {
 		console.log("iwork");
+	}
+
+	const gravityFallsChars = characters.map((chars) => {
+		return (
+			<Card
+				src={chars.charImage}
+				alt={chars.name}
+				key={chars.id}
+				name={chars.name}
+			/>
+		);
+	});
+
+	function shuffle(array) {
+		let currentIndex = array.length,
+			randomIndex;
+		while (currentIndex !== 0) {
+			randomIndex = Math.floor(Math.random() * currentIndex);
+			currentIndex--;
+			[array[currentIndex], array[randomIndex]] = [
+				array[randomIndex],
+				array[currentIndex],
+			];
+		}
+
+		return array;
 	}
 
 	return (
@@ -14,7 +41,9 @@ function App() {
 				<Header />
 				<Score />
 			</div>
-			<Card onClick={handleClick} />
+			<div className="card-game" onClick={handleClick}>
+				{gravityFallsChars}
+			</div>
 		</div>
 	);
 }
--- memory-card/src/Components/Card.js
+++ memory-card/src/Components/Card.js
@@ -1,45 +1,10 @@
 import React from "react";
-import Mapcard from "./Mapcard";
-import characters from "./characterData";
 
 export default function Card(props) {
-	const gravityFallsChars = characters.map((chars) => {
-		return (
-			<Mapcard
-				src={chars.charImage}
-				alt={chars.name}
-				key={chars.id}
-				name={chars.name}
-			/>
-		);
-	});
-
-	const [testNum, setTestNum] = React.useState([
-		1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12,
-	]);
-
-	function shuffle(array) {
-		let currentIndex = array.length,
-			randomIndex;
-		while (currentIndex !== 0) {
-			randomIndex = Math.floor(Math.random() * currentIndex);
-			currentIndex--;
-			[array[currentIndex], array[randomIndex]] = [
-				array[randomIndex],
-				array[currentIndex],
-			];
-		}
-
-		return array;
-	}
-
-	console.log(testNum, "before");
-	console.log(shuffle(testNum), "during");
-	console.log(testNum, "after");
-
 	return (
-		<div className="card-game" onClick={props.onClick}>
-			{gravityFallsChars}
+		<div className="card-container">
+			<img src={props.src} alt={props.alt} />
+			<h3>{props.name}</h3>
 		</div>
 	);
 }
